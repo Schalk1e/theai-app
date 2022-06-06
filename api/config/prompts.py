344f00a7e9f3cfd1d270b0e07d36d76f043af4cd
Schalk1e <schalk@praekelt.org>
2022-06-06 09:06:57 +0200

    Remove long-winded promt. (@Schalk1e)

--- api/config/prompts.py
+++ api/config/prompts.py
@@ -3,5 +3,4 @@ PROMPTS = [
     "There are too many hipster AI startups because:",
     "We should do something about all these hipster AI startups because:",
     "All these hipster AI startups are getting on my nerves because:",
-    "What do you propose we do about all these hipster AI startups?",
 ]
